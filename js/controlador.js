1fc9ff5ac270600812c6688f62959e289506d0e3
Edison Smith Calderón Sánchez <edison.calderon0@gmail.com>
2015-07-20 12:22:27 -0500

    passwordChange implementado. Correcciones en signUp.

--- js/controlador.js
+++ js/controlador.js
@@ -1,34 +1,83 @@
 var app= angular.module("Control-H3",[]);
 
-app.controller("signUpController",function signUpController($scope){
-    $scope.message= "";
+app.controller("signUpController",function signUpController($scope){     
+    $scope.message_title="";
+    $scope.message_content="";
     $scope.newUser={
         name: "",
         pass: ""
-    };
-    $scope.signUp= function(){        
+    };    
+    $scope.signUp= function(){               
         if($scope.newUser.name != ""  && $scope.newUser.pass!=""){
             var info = {
-            ruta : "/user",
-            type : 'POST',
-            data : {
-                "name" : $scope.newUser.name,
-                "password" : $scope.newUser.pass                
-            }           
-            };
-            console.log(info);
-            queryServer(info, function(error, result){                
-                if(error){                    
-                    alert("Error: "+ error);
+                ruta : "/user",
+                type : 'POST',
+                data : {
+                    "name" : $scope.newUser.name,
+                    "password" : $scope.newUser.pass                
                 }           
-                else{                    
-                    alert("User created successfully. Welcome!");
-                    //Se envia la inforamcion del empleado a otra vista 
+            };            
+            queryServer(info, function(error, user){                
+                if(error){     
+                    $scope.message_title="Error";               
+                    $scope.message_content= error.error;
+                }           
+                else{    
+                    $scope.message_title="Message";                
+                    $scope.message_content="User created successfully. Welcome!";
+                    //Se envia la informacion del empleado a otra vista 
                 }
+                $scope.$apply();
             });
         }
         else{
-            alert("Any field may be empty");
+            $scope.message_title="Error";
+            $scope.message_content="Any field may be empty";
+        }       
+    };
+});
+
+app.controller("passwordChangeController",function changePasswordController($scope){
+    $scope.message_title="";
+    $scope.message_content="";
+    $scope.user= {
+        name: "",
+        pass1: "",
+        pass2:"",
+        newPass: ""
+    };
+    $scope.changePass= function(){
+        if($scope.user.name != ""  && $scope.user.pass1!="" && $scope.user.pass2!="" && $scope.user.newPass!=""){
+            if($scope.user.pass1 === $scope.user.pass2){
+                var info = {
+                    ruta : "/passwordChange",
+                    type : 'POST',
+                    data : {
+                       "name" : $scope.user.name,
+                       "password" : $scope.user.pass1,
+                       "newPassword": $scope.user.newPass                
+                    }           
+                };            
+                queryServer(info, function(error, usuario){                                                  
+                    if(error){         
+                        $scope.message_title="Error";
+                        $scope.message_content= error.error;                        
+                    }           
+                    else{   
+                        $scope.message_title= "Message";                 
+                        $scope.message_content= "Password changed successfully";
+                        //Se envia la informacion del empleado a otra vista 
+                    }
+                    $scope.$apply();
+                });
+            }else{
+                $scope.message_title="Error";
+                $scope.message_content= "Old password's don't match";                
+            }
+        }
+        else{
+            $scope.message_title="Error";
+            $scope.message_content= "Any field may be empty";
         }
     };
 });
@@ -43,11 +92,10 @@ function queryServer (info, callback){
         dataType: dataType,
         data: data,
         timeout: 15000,
-        success: function(result) {     
+        success: function(result) { 
             callback(null, result);
          },   
-         error: function(jqXHR, textStatus, error) {
-                
+         error: function(jqXHR, textStatus, error) {               
                 error={
                     jqXHR : jqXHR, 
                     textStatus : textStatus, 
